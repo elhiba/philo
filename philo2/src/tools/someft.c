64bbb02723183f7b2daeef69327c65159d2d2c7c
elhiba <elhibamohamedamine@gmail.com>
2025-07-15 05:27:24 +0100

    update philosophers + Updating struct + Add new logic

--- philo2/src/tools/someft.c
+++ philo2/src/tools/someft.c
@@ -6,7 +6,7 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 20:52:28 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/07/13 20:56:23 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/07/15 04:20:03 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,13 @@ int	ft_atoi(char *number)
 		index++;
 	}
 	return (result);
+}
+
+long long current_time()
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+
+	return (tv.tv_sec * 1000LL + tv.tv_usec / 1000LL);
 }
