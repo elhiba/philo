93cefb93591d36561ba44161a36586b6123626cb
elhiba <elhibamohamedamine@gmail.com>
2025-08-06 14:46:55 +0100

    Small updates

--- src/error.c
+++ src/error.c
@@ -6,16 +6,20 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 02:52:28 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/08/06 10:29:14 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/08/06 14:40:45 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/philo.h"
 
+void	errors_free()
+{
+}
+
 void	errors(t_data *data, char *message)
 {
 	if (message)
-		perror(message);
+		write(2, message, ft_strlen(message));
 	data->failure = 1;
 	return ;
 }
--- src/initializer.c
+++ src/initializer.c
@@ -6,7 +6,7 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 16:31:32 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/08/06 11:13:04 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/08/06 14:44:54 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,19 @@ t_philo	*init_philo(t_data *data)
 	i = 1;
 	ptr = ft_calloc(1, sizeof(t_philo));
 	if (!ptr)
+	{
 		errors(data, "malloc");
+		return NULL;
+	}
 	philo = ptr;
 	while (i < data->nm_philo)
 	{
 		tmp = ft_calloc(1, sizeof(t_philo));
 		if(!tmp)
+		{
 			errors(data, "malloc");
+			return NULL;
+		}
 		ptr->next = tmp;
 		ptr = tmp;
 		i++;
@@ -81,8 +87,11 @@ void	fork_lock(t_data *data, t_philo *philo)
 	i = 0;
 	ptr = philo;
 	pen = ft_calloc(1, sizeof(pthread_mutex_t));
-	if (!pen)
+	if (!philo || !pen)
+	{
 		errors(data, "malloc");
+		return;
+	}
 	pthread_mutex_init(pen, NULL);
 	data->start = false;
 	while (i < data->nm_philo)
@@ -95,7 +104,10 @@ void	fork_lock(t_data *data, t_philo *philo)
 		ptr->data = data;
 		ptr->fork = ft_calloc(1, sizeof(pthread_mutex_t));
 		if (!ptr->fork)
+		{
 			errors(data, "malloc");
+			return;
+		}
 		pthread_mutex_init(ptr->fork, NULL);
 		ptr = ptr->next;
 		i++;
