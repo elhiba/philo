51c6a5a5b6d830057529da4c13c4e6426910505b
elhiba <elhibamohamedamine@gmail.com>
2025-08-09 20:35:16 +0100

    parsing
    + Handling unused characters (that will not break the program but can be
    ignored so i handled it as an error and philos should not break [i take
    it as a challange no more!])

--- src/tools/ft_atoi.c
+++ src/tools/ft_atoi.c
@@ -6,11 +6,25 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 18:59:27 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/08/08 22:58:53 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/08/09 20:27:52 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
+
+int	is_digit(char *input)
+{
+	while (*input)
+	{
+		if (!(*input >= '0' && *input <= '9'))
+		{
+			write(2, IN_NOT_VAL, ft_strlen(IN_NOT_VAL));
+			return (-2);
+		}
+		input++;
+	}
+	return (0);
+}
 
 int	is_valid_input(long num)
 {
@@ -29,6 +43,8 @@ long	ft_atoi(char *num)
 
 	i = 0;
 	result = 0;
+	if (is_digit(num))
+		return (-2);
 	while (num[i] >= '0' && num[i] <= '9')
 	{
 		result = result * 10 + num[i] - 48;
